a2ba74be7e5cda554b630c9674c057e53ab8b9ba
Isaac <isaacallen73@gmail.com>
2018-02-25 10:31:45 -0500

    Learned some front end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let white = 'white';
+let defaultStyle = {
+  color: "#ecebe8"
+};
+
+class Search extends Component {
+  render () {
+    return (
+      <div style={{defaultStyle}}>
+      <img/>
+      <input type="text"/>
+      </div>
+    )
+  }
+}
+
+class User extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "30%", display: "inline-block"}}>
+      <img/>
+      <h3>User</h3>
+      <button>Create Playlist</button>
+      <ul><li>Genre 1</li><li>Genre 2</li><li>Genre 3</li></ul>
+        </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Title</h1>
-        <header className="App-header">
-
+      <h1 style={{color: "#84bd00"}}>instafy</h1>
+      <Search/>
+      <User/><User/><User/>
       </div>
     );
   }
