48709ab7db1342643efb016326a6f0a556693c24
Isaac <isaacallen73@gmail.com>
2018-02-25 14:23:28 -0500

    Sample user data and authentication backend

--- src/App.js
+++ src/App.js
@@ -7,26 +7,67 @@ let defaultStyle = {
   color: "#ecebe8"
 };
 
-class Search extends Component {
+let sampleUserData = [
+  {
+    name: "isaac",
+    email: "isaacallen73@gmail.com",
+    username: "isaacallen73",
+    topArtists: ["Kendrick Lamar", "J. Cole", "Drake"]
+  },
+  {
+    name: "Tyler",
+    email: "tyler@gmail.com",
+    username: "696969",
+    topArtists: ["Drake", "Migos", "Bryson Tiller"]
+  }
+]
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "#84bd00" }}>{sampleUserData[0].name}'s instafy</h1>
+      </div>
+    )
+  }
+}
+
+class TopArtists extends Component {
   render () {
     return (
-      <div style={{defaultStyle}}>
-      <img/>
-      <input type="text"/>
+      <div>
+        <h3>
+          <ul>
+            <li>{sampleUserData[0].topArtists[0]}</li>
+            <li>{sampleUserData[0].topArtists[1]}</li>
+            <li>{sampleUserData[0].topArtists[2]}</li>
+          </ul>
+        </h3>
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <div style={{ defaultStyle }}>
+        <img />
+        <input type="text" />
       </div>
     )
   }
 }
 
 class User extends Component {
-  render () {
+  render() {
     return (
-      <div style={{...defaultStyle, width: "30%", display: "inline-block"}}>
-      <img/>
-      <h3>User</h3>
-      <button>Create Playlist</button>
-      <ul><li>Genre 1</li><li>Genre 2</li><li>Genre 3</li></ul>
-        </div>
+      <div style={{ ...defaultStyle, width: "30%", display: "inline-block" }}>
+        <img />
+        <h3>User</h3>
+        <button>Create Playlist</button>
+        <ul><li>Genre 1</li><li>Genre 2</li><li>Genre 3</li></ul>
+      </div>
     )
   }
 }
@@ -35,9 +76,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1 style={{color: "#84bd00"}}>instafy</h1>
-      <Search/>
-      <User/><User/><User/>
+        <Title/>
+        <TopArtists/>
+        <Search/>
+        <User/><User/><User/>
       </div>
     );
   }
